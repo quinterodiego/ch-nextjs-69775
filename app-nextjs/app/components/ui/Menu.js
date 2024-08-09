2f4e8c78b8e915e87cc01982bc5b04290eca4637
quinterodiego <d86webs@gmail.com>
2024-08-08 23:34:14 -0300

    primera subida

--- app-nextjs/app/components/ui/Menu.js
+++ app-nextjs/app/components/ui/Menu.js
@@ -11,8 +11,8 @@ export const Menu = () => {
   const handleClose = () => setOpen(!open)
 
   return (
-    <div onClick={hanleOpen} className='hover:border hover:rounded-lg p-3 hover:cursor-pointer'>
-      <Image src={'/main-menu.png'} alt='menu img' height={30} width={30} />
+    <div onClick={hanleOpen} className='border rounded-lg p-3'>
+      <Image src={'/main-menu.png'} alt='menu img' height={30} width={30} className='hover:opacity-75 hover:cursor-pointer' />
       <MenuList handleClose={handleClose} open={open} />
     </div>
   )
--- app-nextjs/app/productos/[detalle]/page.js
+++ app-nextjs/app/productos/[detalle]/page.js
@@ -18,17 +18,7 @@ const Detalle = () => {
 
   return (
     <div className="bg-white">
-      <div className="pt-6">
-        <nav aria-label="Breadcrumb">
-          <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-            <li className="text-sm">
-              <a href={product.slug} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
-                {product.title}
-              </a>
-            </li>
-          </ol>
-        </nav>
-
+      <div className="">
         {/* Image gallery */}
         <div className="mx-auto mt-6 max-w-full sm:px-6 lg:grid lg:max-w-full lg:grid-cols-3 lg:gap-x-8 lg:px-8">
           {
