3b02ae90840a8f40294d8d98210f0bcff8ca4c32
quinterodiego <d86webs@gmail.com>
2024-09-02 19:37:52 -0300

    consumo productos de firebase

--- e-commerce/app/page.js
+++ e-commerce/app/page.js
@@ -1,6 +1,8 @@
 'use client'
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import ProductList from './productos/ProductList.js';
 import { useEffect, useState, Suspense } from 'react';
+import { db } from './config/firebase.js';
 
 const categories = [
   { name: "Todos"},
@@ -18,9 +20,21 @@ const Home = () => {
   const [categoryIndex, setCategoryIndex] = useState(0)
 
   const getProdyctByCategory =  async(category) => {
-    const data = await fetch(`http://localhost:3000/api/productos/${category}`)
-    const products = await data.json()
-    setproducts(products)
+    try {
+      const productRef = collection(db, 'products')
+      let q
+      if(category.toLowerCase() === 'todos') {
+        q = query(productRef)
+      } else {
+        q = query(productRef, where('category', '==', category))
+      }
+      const querySnapshots = await getDocs(q)
+      const docs = querySnapshots.docs.map(doc => doc.data())
+      console.log('docs', docs)
+      setproducts(docs)
+    } catch (error) {
+      
+    }
   }
 
   useEffect(() => {
@@ -28,6 +42,7 @@ const Home = () => {
   }, [selected])
 
   const onSelectCategory = (category, index) => {
+    console.log(category, index)
     getProdyctByCategory(category)
     setSelected(category)
     setCategoryIndex(index)
@@ -38,7 +53,7 @@ const Home = () => {
       <aside className='px-6'>
         <h3 className='text-xl pb-2'>Categorias</h3>
         <ul>
-          {categories.map((category, index) => <li className={`cursor-pointer py-1 hover:opacity-75 ${categoryIndex === index ? 'text-blue-400' : ''}`} onClick={() => onSelectCategory(category.name, index)}>{category.name}</li>)}
+          {categories.map((category, index) => <li key={index} className={`cursor-pointer py-1 hover:opacity-75 ${categoryIndex === index ? 'text-blue-400' : ''}`} onClick={() => onSelectCategory(category.name, index)}>{category.name}</li>)}
         </ul>
       </aside>
       <Suspense fallback={<div>Cargando...</div>}>
